b4fdcecee69c1e434d73d39f9e2b38c0de24218b
Mark <mrkrphl0705@gmail.com>
2022-04-30 20:21:49 +0800

    webhook

--- start_bot.py
+++ start_bot.py
@@ -1,3 +1,7 @@
+import os
 def start_bot(updater):
+    updater.start_webhook(listen="0.0.0.0",
+                          port=int(int(os.environ.get('PORT', 5000))),
+                          url_path='5084650563:AAGoOgqgmT9gNDSKt_LC5VmiITXPhrWUpsg')
     updater.bot.setWebhook('https://gentle-retreat-84776.herokuapp.com/' + '5084650563:AAGoOgqgmT9gNDSKt_LC5VmiITXPhrWUpsg')
-    updater.start_polling()
+    
